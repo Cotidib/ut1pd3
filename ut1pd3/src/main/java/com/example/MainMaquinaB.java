23b86f1e50212742ecdf8c9df5cd1718f93a8628
Cotidib <constanza.dibueno@gmail.com>
2024-03-23 17:30:36 -0300

    Paso 2 Maquina B

--- ut1pd3/src/main/java/com/example/MainMaquinaB.java
+++ ut1pd3/src/main/java/com/example/MainMaquinaB.java
@@ -5,6 +5,13 @@ public class MainMaquinaB {
         Gato gato = new Gato("Garfield");
         gato.maullar();
         System.out.println("El nombre del gato es " + gato.getNombre());
-        System.out.println("El gato responde a gato? " + gato.respondeA("gato"));
+        System.out.println("El gato responde a Garfield? " + gato.respondeA("Garfield"));
+        System.out.println("El gato responde a Gato? " + gato.respondeA("Gato"));
+
+        Perro perro = new Perro("Cliford");
+        perro.ladrar();
+        System.out.println("El nombre del perro es " + perro.getNombre());
+        System.out.println("El perro responde a Cliford? " + perro.respondeA("Cliford"));
+        System.out.println("El perro responde a Carlos? " + perro.respondeA("Carlos"));
     }
 }
